5ef39140d72965ea5d04221ac07d20a3ff5e38ad
nithyamajeti06 <majetinithya@gmail.com>
2026-02-13 23:08:36 -0500

    adding the new script for the model made by the team

---
+++ simple_script/newScript.py
@@ -0,0 +1,35 @@
+import os
+import re
+import html
+
+defaultFile = "examples/new/model/scenario.drawio.xml" 
+output = "examples/new/src-gen" 
+
+os.makedirs(output, exist_ok=True)
+
+with open(defaultFile, "r") as file:
+    for line in file:
+        # skip edgeLabel, must be vertex with text
+        if line.find('vertex="1"') != -1 and line.find('value=') != -1 and line.find('edgeLabel') == -1:
+            start = line.find('value="') + 7
+            end = line.find('"', start)
+            # no whitespaces
+            raw = line[start:end]
+            raw = html.unescape(raw)
+            raw = re.sub(r'<.*?>', '', raw)
+            className = raw.strip()
+
+            # when saying the class name only should be letters, numbers, underscores
+            className = re.sub(r'[^0-9a-zA-Z_]', '', className)
+
+            # drives1 keeps making a class -> not supposed to
+            if className == "":
+                continue
+
+            if className[0].isalpha():
+                # create java file path
+                classPath = os.path.join(output, f"{className}.java")
+                with open(classPath, "w") as classFile:
+                    # basic class 
+                    classFile.write(f"public class {className} {{\n}}\n")
+print("Done!")
